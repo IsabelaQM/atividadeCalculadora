e75ee5d001830a84b884082112b354cc691f4c3a
Isabela Queiroz <106333972+IsabelaQM@users.noreply.github.com>
2024-08-26 23:20:36 -0300

    Create ExponenciarException.java

---
+++ calculadora/src/main/java/com/example/calculadora/ExponenciarException.java
@@ -0,0 +1,12 @@
+package com.example.calculadora;
+
+
+public class ExponenciarException extends Exception {
+
+    public ExponenciarException(String message) {
+
+        super(message);
+
+    }
+
+}
